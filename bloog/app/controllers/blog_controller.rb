882ff52b3714685f8455c36b295de2a8591dd892
Ed van Beinum <ed@vanbeinum.org>
2012-05-30 21:05:16 +0100

    completed 5-2

--- bloog/app/controllers/blog_controller.rb
+++ bloog/app/controllers/blog_controller.rb
@@ -1,5 +1,12 @@
 class BlogController < ApplicationController
   def index
     @blog = Blog.new
+    post1 = @blog.new_post
+    post1.title = "Paint just applied"
+    post1.body = "Paint just applied. It's a lovely orangey-purple!"
+    post1.publish
+    post2 = @blog.new_post(title: "Still Wet")
+    post2.body = " Paint still quite wet, no bubbling"
+    post2.publish
   end
 end
--- bloog/app/models/blog.rb
+++ bloog/app/models/blog.rb
@@ -1,12 +1,27 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_source
+
   def initialize
     @entries = []
   end
+
   def title
     "Watching Paint Dry"
   end
+
   def subtitle
     "The trusted source for paint drying news and opinion"
+  end
+
+  def new_post
+    post_source.call.tap do |p|
+      p.blog = self
+    end
+  end
+
+  private
+  def post_source
+    @post_source ||= Post.public_method(:new)
   end
 end
